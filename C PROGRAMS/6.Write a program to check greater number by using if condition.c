b529ac98da0c8241cd12abf6d1238ea653b78155
Surya8114 <111639599+Surya8114@users.noreply.github.com>
2022-08-21 18:45:44 +0530

    Create 6.Write a program to check greater number by using if condition.c

---
+++ C PROGRAMS/6.Write a program to check greater number by using if condition.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+main() 
+{
+  int a, b;
+  printf("Please enter the value of a and b: ");
+  scanf("%d%d",&a, &b);
+  if (a > b) 
+  {
+    printf("%d is greater.", a);
+  }
+  if (b > a) 
+  {
+    printf("%d is greater.", b);
+  }
+  if (a == b) 
+  {
+    printf("%d and %d are equal.", a, b);
+  }
+} 
+#include <stdio.h>
+main() 
+{
+  int a, b;
+  printf("Please enter the value of a and b: ");
+  scanf("%d%d",&a, &b);
+  if (a > b) 
+  {
+    printf("%d is greater.", a);
+  }
+  if (b > a) 
+  {
+    printf("%d is greater.", b);
+  }
+  if (a == b) 
+  {
+    printf("%d and %d are equal.", a, b);
+  }
+} 
